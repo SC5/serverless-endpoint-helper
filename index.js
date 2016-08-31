e2205ea1e736c098d37903dc1ca740380c251f1d
Gibran Rodriguez <gibran.rodriguez@sc5.io>
2016-08-31 14:21:33 +0300

    Start support for serverless 1.0

--- index.js
+++ index.js
@@ -1,147 +1,77 @@
 'use strict';
 
-
 const path  = require('path'),
   fs        = require('fs'),
-  jsonfile = require('jsonfile'),
-  BbPromise = require('bluebird'); // Serverless uses Bluebird Promises and we recommend you do to because they provide more than your average Promise :)
+  BbPromise = require('bluebird'); 
 
-module.exports = function(S) { // Always pass in the ServerlessPlugin Class
+class EndpointHelper {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
 
-  /**
-   * Adding/Manipulating Serverless classes
-   * - You can add or manipulate Serverless classes like this
-   */
-
-  S.classes.Project.newStaticMethod     = function() { console.log("A new method!"); };
-  S.classes.Project.prototype.newMethod = function() { S.classes.Project.newStaticMethod(); };
-
-  /**
-   * Extending the Plugin Class
-   * - Here is how you can add custom Actions and Hooks to Serverless.
-   * - This class is only required if you want to add Actions and Hooks.
-   */
-
-  class PluginBoilerplate extends S.classes.Plugin {
-
-    constructor() {
-      super();
-      this.name = 'io.sc5.endpoint.helper';
-    }
-
-    registerActions() {
-
-      S.addAction(this._customAction.bind(this), {
-        handler:       'customAction',
-        description:   'An action to create endpoint template',
-        context:       'endpoint',
-        contextAction: 'create',
-        options:       [{
-          option:      'option',
-          shortcut:    'o',
-          description: 'test option 1'
-        }],
-        parameters: [
-          {
-            parameter: 'paths',
-            description: 'One or multiple paths to your function',
-            position: '0->'
-          }
-        ]
-      });
-
-      return BbPromise.resolve();
-    }
-
-    registerHooks() {
-      return BbPromise.resolve();
-    }
-
-    _customAction(evt) {
-
-      let _this = this;
-
-      return new BbPromise(function (resolve, reject) {
-
-        if (S.getProject().getFunction(evt.options.paths[0]) === undefined) {
-          return new BbPromise(function(resolve, reject) {
-          reject(`Endpoint Helper: Function ${evt.options.paths[0]} does not exist in your project`);
-        });
-        }
-
-        return addEndPoint(evt);
-
-      });
-    }
-  }
-
-  function addEndPoint(evt){
-      //Get function object
-      var sFunFilePath = S.getProject().getFunction(evt.options.paths[0]);
-      
-      //Get function path string
-      var funPathString = sFunFilePath.getFilePath();
-
-      // Read current content
-      var funContent = fs.readFileSync(funPathString);  
-      
-      // Parse it
-      var jsonContent = JSON.parse(funContent);
-      
-      //Input path endpoint
-      var funPath = evt.options.paths[1];
-
-      //Input Method      
-      var funMethod = evt.options.paths[2];
-
-      //Add new endpoint
-      jsonContent.endpoints.push(newEndPoint(funPath, funMethod));
-      updateFunction(jsonContent, funPathString);
-      console.log('-------------------');
-      console.log("Endpoint created" + " - " + "Path: " + funPath  +" - " + "Method: " + funMethod );
-      console.log('-------------------');
-  }
-
-  function updateFunction(content, file){
-      jsonfile.spaces = 4
-      jsonfile.writeFile(file, content, function (err) {
-          if(err) {
-              return console.log(err);
-          }
-          console.log("Error saving file, enpoint was not created!");
-      });
-  }
-
-  function newEndPoint(path, method) {
-      return {
-
-      "path": path,
-      "method": method,
-      "type": "AWS",
-      "authorizationType": "none",
-      "authorizerFunction": false,
-      "apiKeyRequired": false,
-      "requestParameters": {},
-      "requestTemplates": "",
-      "responses": {
-        "400": {
-          "statusCode": "400"
-        },
-        "default": {
-          "statusCode": "200",
-          "responseParameters": {},
-          "responseModels": {
-            "application/json;charset=UTF-8": "Empty"
-          },
-          "responseTemplates": {
-            "application/json;charset=UTF-8": ""
+    this.commands = {
+      create: {
+        usage: 'Create new endpoint for your function',
+        commands: {
+          endpoint: {
+            usage: 'Create endpoint',
+            lifecycleEvents: [
+              'endpoint'
+            ],
+            options: {
+              function: {
+                usage: 'Name of the function',
+                shortcut: 'f',
+                required: true,
+              },
+               method: {
+                usage: 'Provide a method',
+                shortcut: 'm',
+                required: true,
+              },
+              path: {
+                usage: 'Provide a path',
+                shortcut: 'p',
+                required: false,
+              }
+            }
           }
         }
       }
     };
 
+    this.hooks = { 
+      'create:endpoint:endpoint':() => {
+        BbPromise.bind(this)
+        .then(this.addEndPoint());
+       }
+     }
   }
 
-  // Export Plugin Class
-  return PluginBoilerplate;
-};
+  addEndPoint(){
+      //Get function object
+      let funcName = this.options.f || this.options.function;
+      let funPath = this.options.p || this.options.path;
+      let funMethod = this.options.m || this.options.method;
+      let _this = this;
+      
+      console.log('-------------------');
+      console.log("Endpoint created" + " - " + "function: " + funcName +
+                    " - " + "method: " + funMethod  + " - " + "path: " + funPath);
+      console.log('-------------------');
+  }
+
+/*
+  updateFunction(content, file){
+  
+  }
+
+  newEndPoint(path, method) {
+
+
+  }
+  */
+
+}
+
+module.exports = EndpointHelper;
